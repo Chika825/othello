8d6ce20609dd66cf5700c42f28f0b383a51f43ab
Chika825 <lightgreen726@gmail.com>
2024-04-23 08:23:08 +0900

    色のダブり防止

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,15 +16,79 @@ const Home = () => {
   const clickHandler = (x: number, y: number) => {
     console.log(x, y);
     const newBoard = structuredClone(board);
+    /*
+    const directions = [
+      [0, 1],
+      [0, -1],
+      [-1, 0],
+      [1, 0],
+      [-1, -1],
+      [1, 1],
+      [-1, 1],
+      [1, -1],
+    ];
+    directions.forEach(function (zh) {
+      if (board[y + zh[1]] !== undefined && board[y + zh[1]][x + zh[0]] === 3 - turnColor) {
+        console.log(y + zh[1], x + zh[0]);
+        newBoard[y][x] = turnColor;
+        setTurnColor(3 - turnColor);
+        setBoard(newBoard);
+      } else if (board[x + zh[0]] !== undefined && board[y + zh[1]][x + zh[0]] === 3 - turnColor) {
+        console.log(y + zh[1], x + zh[0]);
+        newBoard[y][x] = turnColor;
+        setTurnColor(3 - turnColor);
+        setBoard(newBoard);
+      }
+    });
+    */
+    /*
+    directions.forEach(function (zh) {
+      if (board[y + zh[1]][x + zh[0]] === 3 - turnColor) {
+        newBoard[y + zh[1]][x + zh[0]] = turnColor;
+        setBoard(newBoard);
+      }
+    });
+  */
 
-    if (board[y + 1] !== undefined && board[y + 1][x] === 3 - turnColor) {
-      newBoard[y][x] = turnColor;
-      setTurnColor(3 - turnColor);
-      setBoard(newBoard);
+    {
+      if (board[y + 1] !== undefined && board[y + 1][x] === 3 - turnColor) {
+        newBoard[y][x] = turnColor;
+        setTurnColor(3 - turnColor); //いかに分岐をなくすか
+        // turnColor === 1 ? setTurnColor(2) : setTurnColor(1);
+        // setTurnColor(turnColor === 1 ? 2 : 1);
+        // setBoard(newBoard);
+      } else if (board[y - 1] !== undefined && board[y - 1][x] === 3 - turnColor) {
+        newBoard[y][x] = turnColor;
+        setTurnColor(3 - turnColor);
+        //setBoard(newBoard);
+      } else if (board[x + 1] !== undefined && board[y][x + 1] === 3 - turnColor) {
+        newBoard[y][x] = turnColor;
+        setTurnColor(3 - turnColor);
+        // setBoard(newBoard);
+      } else if (board[x - 1] !== undefined && board[y][x - 1] === 3 - turnColor) {
+        newBoard[y][x] = turnColor;
+        setTurnColor(3 - turnColor);
+        // setBoard(newBoard);
+      } /*else if (board[y + 1][x + 1] !== undefined && board[y + 1][x + 1] === 3 - turnColor) {
+        newBoard[y][x] = turnColor;
+        setTurnColor(3 - turnColor);
+        // setBoard(newBoard);
+      } else if (board[y - 1][x + 1] !== undefined && board[y - 1][x + 1] === 3 - turnColor) {
+        newBoard[y][x] = turnColor;
+        setTurnColor(3 - turnColor);
+        // setBoard(newBoard);
+      } else if (board[y + 1][x - 1] !== undefined && board[y + 1][x - 1] === 3 - turnColor) {
+        newBoard[y][x] = turnColor;
+        setTurnColor(3 - turnColor);
+        // setBoard(newBoard);
+      } else if (board[y - 1][x - 1] !== undefined && board[y - 1][x - 1] === 3 - turnColor) {
+        newBoard[y][x] = turnColor;
+        setTurnColor(3 - turnColor);
+        // setBoard(newBoard);
+      }*/
     }
-    // turnColor === 1 ? setTurnColor(2) : setTurnColor(1);
-    // setTurnColor(turnColor === 1 ? 2 : 1);
-    //setTurnColor(3 - turnColor); いかに分岐をなくすか
+    setBoard(newBoard);
+
     //setBoard(newBoard);
     //const directions
     //1.めくれる、点数 2.候補地 3.パス、2回パス
