7a289e78c033314e88db18785585945d475b10ad
Chika825 <lightgreen726@gmail.com>
2024-05-14 21:48:01 +0900

    prettier

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -93,7 +93,7 @@ const Home = () => {
                   }
                 }
               } else {
-                flag = false
+                flag = false;
                 break;
               }
             }
